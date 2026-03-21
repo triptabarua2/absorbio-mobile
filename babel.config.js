39fd56226775de7a55f608c62d5aed23c70bb063
triptabarua2 <252576309+triptabarua2@users.noreply.github.com>
2026-03-21 09:14:17 -0400

    fix: update react-native-reanimated to ~4.1.1 and clean up babel.config.js to fix EAS build failure

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
-  // এই line টা পরিবর্তন করুন:
-  // plugins.push("react-native-worklets/plugin");  ← পুরনো, কাজ করে না
-  plugins.push("react-native-reanimated/plugin");   // ← এটা দিন
-
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-    plugins,
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
