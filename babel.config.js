26760e457cd132bf4c375903d9c416098bd8ca19
triptabarua2 <triptabarua2@gmail.com>
2026-03-21 19:04:34 +0600

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   let plugins = [];
 
-  plugins.push("react-native-worklets/plugin");
+  // এই line টা পরিবর্তন করুন:
+  // plugins.push("react-native-worklets/plugin");  ← পুরনো, কাজ করে না
+  plugins.push("react-native-reanimated/plugin");   // ← এটা দিন
 
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
